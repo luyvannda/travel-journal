c4c330a313dab506c0d26913fe07c985766016cb
luyvannda <philips.joe329@gmail.com>
2023-11-22 15:52:41 +0700

    add image to Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import Header from './components/Header'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card() {
+  return (
+    <div id="card-component" className="container mt-4">
+      <img id="card-image" className="col-6 pt-2 rounded-1 mx-auto d-block" src="/mount-fuji.png" alt="location image"></img>
+    </div>
+  )
+}
