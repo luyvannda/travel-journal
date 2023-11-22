2bb040178c4569ab41dec153a6ba7a37353d70fa
luyvannda <philips.joe329@gmail.com>
2023-11-22 15:11:24 +0700

    complete header component with Inter font and color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import globeIcon from "/small-globe-icon.png"
 export default function Header() {
   return (
     <>
-      <div id="header-component" className="container-fluid pt-3 pb-3 bg-danger">
-        <h5 className="text-center text-white"><img className="pe-2" src={globeIcon} alt="A globe icon"></img>my travel journal.</h5>
+      <div id="header-component" className="container-fluid pt-3 pb-3">
+        <h5 className="text-center text-white fw-medium"><img className="pe-2" src={globeIcon} alt="A globe icon"></img>my travel journal.</h5>
       </div>
     </>
   )
