89c5d72a156c08bf8178190b500a7854970e1351
luyvannda <philips.joe329@gmail.com>
2023-11-23 15:02:23 +0700

    add two more data as props

--- src/components/cardData.jsx
+++ src/components/cardData.jsx
@@ -7,6 +7,25 @@ const cardData = [
     title: "Mount Fuji",
     date: "12 Jan, 2021 - 24 Jan, 2021",
     description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+  },
+
+  {
+    id: 2,
+    imgCover: "https://source.unsplash.com/JmuyB_LibRo",
+    country: "AUSTRALIA",
+    googleMap: "https://maps.app.goo.gl/9PeZSvqz9m5MqpVw7",
+    title: "Sydney Opera House",
+    date: "27 May, 2021 - 8 Jun, 2021",
+    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+  },
+  {
+    id: 3,
+    imgCover: "https://source.unsplash.com/3PeSjpLVtLg",
+    country: "NORWAY",
+    googleMap: "https://maps.app.goo.gl/9Ed3DtBsqCSWRU4TA",
+    title: "Geirangerfjord",
+    date: "01 Oct, 2021 - 18 Nov, 2021",
+    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality. "
   }
 ]
 
