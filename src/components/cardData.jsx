ce72211b403ee4c664b3f50d5282fce7bebfc748
luyvannda <philips.joe329@gmail.com>
2023-11-24 10:59:09 +0700

    add three more travel destination to Card components' data

--- src/components/cardData.jsx
+++ src/components/cardData.jsx
@@ -26,6 +26,36 @@ const cardData = [
     title: "Geirangerfjord",
     date: "01 Oct, 2021 - 18 Nov, 2021",
     description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality. "
+  },
+
+  {
+    id: 4,
+    imgCover: "https://images.unsplash.com/photo-1577931683033-1059552104e0?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    country: "MALAYSIA",
+    googleMap: "https://maps.app.goo.gl/C2cbfuYadigVFh3Q9",
+    title: "Petronas Twin Towers",
+    date: "02 Mar, 2022 - 20 Mar, 2022",
+    description: "The Petronas Towers, also known as the Petronas Twin Towers and colloquially the KLCC Twin Towers, are an interlinked pair of 88-storey supertall skyscrapers in Kuala Lumpur, Malaysia, standing at 451.9 metres (1,483 feet)."
+  },
+
+  {
+    id: 5,
+    imgCover: "https://statics.vinpearl.com/bui-vien-street-1_1692545346.jpg",
+    country: "VIETNAM",
+    googleMap: "https://maps.app.goo.gl/n5acjztFZjnhgMXd7",
+    title: "Bui Vien Walking Street",
+    date: "26 Nov, 2022 - 29 Nov, 2022",
+    description: "Bui Vien Street is known as the 'Western Street' (Pho Tay) because of its Western culture-oriented features. This street is always crowded with backpackers who come here to have fun, try local cuisines, and explore new places during their trip to Saigon."
+  },
+
+  {
+    id: 6,
+    imgCover: "https://images.unsplash.com/photo-1566706546199-a93ba33ce9f7?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    country: "CAMBODIA",
+    googleMap: "https://maps.app.goo.gl/gXgDdQyk83eTy5g3A",
+    title: "Angkor Wat",
+    date: "18 Feb, 2023 - 21 Feb, 2023",
+    description: "Angkor is one of the most important archaeological sites in South-East Asia. Stretching over some 400 km2, including forested area, Angkor Archaeological Park contains the magnificent remains of the different capitals of the Khmer Empire."
   }
 ]
 
